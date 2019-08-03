4d1b1744a9ada809f70c3ba17ff079d59322da23
ChynybekKazakbayev <chynybekkazakbaevuulu@gmail.com>
2019-08-03 20:23:17 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,17 @@
+public class Hero {
+
+    int health ;
+    int hit ;
+    int damage ;
+
+    public Hero (int health, int hit, int damage){
+        this. health = health;
+        this. hit = hit;
+        this. damage = damage;
+    }
+
+
+
+
+
+}
